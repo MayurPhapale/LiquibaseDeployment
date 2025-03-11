c14a6738ff7514dca2df6b83a73e2814fd8c3796
MayurPhapale <69360167+MayurPhapale@users.noreply.github.com>
2025-03-11 14:43:02 +0530

    Update change2.sql

--- dbscripts/change2.sql
+++ dbscripts/change2.sql
@@ -7,3 +7,8 @@ CREATE TABLE test_table2 (
     test_name VARCHAR(255),
     PRIMARY KEY (test_id)
 );
+-- rollback BEGIN
+INSERT INTO liquibase_audit (action, changeset_id, timestamp) 
+VALUES ('ROLLBACK', 'mayur_phapale-name:3', CURRENT_TIMESTAMP);
+DROP TABLE test_table2;
+-- rollback END;
